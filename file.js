6d0f5408330022637d79ed0d90d8198a952f152f
noutcheu libert <joran.noutcheu@facsciences-uy1.cm>
2023-08-18 16:27:34 +0100

    end with js form validation and start the card

--- file.js
+++ file.js
@@ -1,91 +1,70 @@
 let form =  document.querySelector("#Myform");
-console.log(form);
-
 const WRONG_FORMAT = "Wrong Format, number only";
 const BLANK = "Can't be blank";
-
-
-function check_name(name) {
-    if(hasValue(name)){
-        removeError(name);
-        return true;
-    }
-    showError(name, BLANK);
-    return false
-
-}
-
-
-function check_card_number(number) {
-    if (Number.isNaN(parseInt(number))){
-        return false;
-    }
-    return true;
-}
-
-
-function check_number(number){
-    if (hasValue(number.value)){
-        if(check_card_number(number.value)){
-            showError(month, WRONG_FORMAT);
-            return false;
-        }
-        removeError(number);
-        return true;
-    }
-
-    showError(month, BLANK);
-    return false;
-        
-}
-
-
-function check_cvc(cvc){
-    if (check_number(cvc)) {
-        if (cvc.value.length == 3) {
-            removeError(cvc)
-            return true;
-        }
-    }
-    showError(cvc, "no more or less than three digit");
-    return false
-
-}
-
+let formCard = document.querySelector("#form-card")
 form.addEventListener("submit", (event)=>{
+    event.preventDefault();
     // stop form submission
 	
    let name = form['name'];
-   
+   console.log(name);
    let card_number = form['number_card'];
    let month = form['mm'];
    let years = form['yy'];
    let cvc = form['cvc'];
-   console.log(form['name']);
-   if (
-    check_card_number(month) && check_card_number(years) && 
-    check_card_number(card_number) && check_name(name) && check_cvc(cvc)
-    ){
-        return event.currentTarget();
+   send_form = true;
+   if (!check_name(name)) {
+        send_form = send_form&&false
+   }
+   if (!check_date_and_cvc(card_number, 16)) {
+    send_form = send_form&&false
+   } 
+    if (!check_date_and_cvc(month, 2)){
+        send_form = send_form&&false
+    } 
+   if (!check_date_and_cvc(years, 2)){
+        send_form = send_form&&false
+    } 
+   
+   if (!check_date_and_cvc(cvc, 3)){
+        send_form = send_form&&false
     }
-   return false;
+    if(send_form){
+        formCard.innerHTML = `<div class="congratulation-card">
+        <div class="logo"></div>
+        <h1>Thank you!</h1>
+        <p class="descritpion">We've added your card details</p>
+        <div class="send">
+          <button type="submit" href="#form-card">Continue</button>
+        </div>
+      </div>`
+        
+        form.submit();
+    }
 });
 
+
 function showMessage(input, message, type){
-    input.parentNode.querySelector("small");
+    let msg = input.parentNode.querySelector("small");
 	msg.innerText = message;
+    console.log(input, msg.innerText);
 }
 
 function showError(input, message){
+    let haveError = input.classList.contains('error')
+    if(!haveError) input.classList.add('error');
     showMessage(input, message, true);
 }
 function removeError(input){
-    input.parentNode.querySelector("small");
+    let haveError = input.classList.contains('error')
+    if(haveError) input.classList.remove('error');
+    let msg =  input.parentNode.querySelector("small");
 	msg.innerText = "";
+
 }
 
 function hasValue(input){
-    if (input.value.trim() === "") {
+    if (input.value === "") {
         return false;
     }
     return true;
@@ -108,3 +87,79 @@ function splitNumber(chaine, number_split){
     }
     return result;
 }
+
+
+
+function check_name(name) {
+    if(hasValue(name)){
+        removeError(name);
+        return true;
+    }
+
+    console.log("show error");
+    showError(name, BLANK);
+    return false
+
+}
+
+
+function checkNumberFormat(number) {
+    
+    const regex = new RegExp("[a-zA-Z\W//]"); 
+    // console.log("pattern find "+ number+" = "+ regex.test(number));
+    if (regex.test(number)){
+        // console.log("good"+number);
+        return false;
+    }
+    // console.log("fake"+number);
+    return true;
+}
+
+
+function check_date_and_cvc(cvc, number_digit){
+    if (hasValue(cvc)){
+        if(containtWrongNumber(cvc)){
+            showError(cvc, WRONG_FORMAT);
+            return false;
+        }
+        if (!checkDigitLength(cvc, number_digit)) {
+            showError(cvc, "no more or less than "+number_digit+" digit"); 
+            return false;
+        }
+        else{
+            removeError(cvc);
+            return true;
+        
+        }
+        
+    }
+        
+    
+
+    showError(cvc, BLANK);
+    return false
+
+}
+
+
+function containtWrongNumber(cvc) {
+    if (checkNumberFormat(cvc.value)) {
+        return false;
+    }   
+        
+        return true;
+}
+
+function checkDigitLength(cvc, number_digit) {
+    let str = cvc.value.replaceAll(' ' , '')
+    if (str.length != number_digit) {
+           
+        return false;
+    }
+
+    removeError(cvc);
+    return true;
+
+    
+}
+
